2b14eb4358be278e5816a8a0b6659cba678475be
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-23 12:44:13 +0200

    esercizio completo

---
+++ snack object 1/js/script.js
@@ -0,0 +1,84 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+const automobili = [
+  {
+    marca: 'Audi',
+    'modello': 'A1',
+    alimentazione: 'diesel',
+  },
+  {
+    marca: 'Tesla',
+    'modello': 'Tesla Model S',
+    alimentazione: 'benzina',
+  },
+  {
+    marca: 'Ford',
+    'modello': 'Ford Mustang', 
+    alimentazione: 'elettrica',
+  },
+  {
+    marca: 'BMW',
+    'modello': 'BMW Serie 3',
+    alimentazione: 'diesel',
+  },
+  {
+    marca: 'Toyota',
+    'modello': 'Toyota Corolla',
+    alimentazione: 'gpl',
+  },
+  {
+    marca:'Honda',
+    'modello':'Honda Civic',
+    alimentazione: 'Metano',
+  },
+  {
+    marca: 'Audi',
+    'modello': 'A4',
+    alimentazione: 'diesel',
+  },
+  {
+    marca: 'Volkswagen',
+    'modello': 'Polo',
+    alimentazione: 'diesel',
+  },
+  {
+    marca: 'Volkswagen',
+    'modello': 'Polo',
+    alimentazione: 'benzina',
+  },
+  {
+    marca: 'Volkswagen',
+    'modello': 'Golf',
+    alimentazione:'elettrica',
+  },
+  {
+    marca: 'Jeep',
+    'modello': 'Jeep Wrangler',
+    alimentazione: 'diesel',
+  },
+  {
+    marca: 'Lamborghini',
+    'modello': 'Lamborghini Huracan',
+    alimentazione: 'benzina',
+  },
+  {
+    marca: 'Nissan',
+    'modello': 'Nissan GT-R',
+    alimentazione: 'diesel',
+  },
+  {
+    marca: 'Porsche',
+    'modello': 'Porsche 911',
+    alimentazione: 'benzina',
+  },
+  {
+    marca: 'Volkswagen',
+    'modello': 'Volkswagen Golf',
+    alimentazione: 'diesel',
+  }
+];
+
+console.log (automobili);
---
+++ snack object 2/js/script.js
@@ -0,0 +1,70 @@
+
+// Crea un array di oggetti che rappresentano delle persone, 10.
+// Ogni persona ha un nome, un cognome e un'età.
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+const listaPersone = [
+  {
+    nome: 'Lucia',
+    cognome: 'Rossi',
+    età: 19,
+  },
+  {
+    nome: 'Luca',
+    cognome: 'Marino',
+    età: 30,
+  },
+  {
+    nome: 'Bendetta',
+    cognome: 'Barbone',
+    età: 21,
+  },
+  {
+    nome: 'Katia',
+    cognome: 'Cataldo',
+    età: 14,
+  },
+  {
+    nome: 'Ludovica',
+    cognome: 'Picariello',
+    età: 50,
+  },
+  {
+    nome: 'Antonio',
+    cognome: 'Russo',
+    età: 12,
+  },
+  {
+    nome: 'Vito',
+    cognome: 'Dillaio',
+    età: 62,
+  },
+  {
+    nome: 'Andrea',
+    cognome: 'Finelli',
+    età: 10,
+  },
+  {
+    nome: 'Michela',
+    cognome: 'prizio',
+    età: 22,
+  },
+  {
+    nome: 'Michele',
+    cognome: 'Cucciniello',
+    età: 13,
+  }
+];
+
+console.log(listaPersone);
+
+
+listaPersone.forEach ((element, index)=>{
+  const minori = [element.età];
+  if (minori < 18){
+    minori.push('non può guidare!');
+  }
+  console.log ('nome'+ element.nome, 'cognome'+ element.cognome, 'età'+ element.età,minori );
+});
+
